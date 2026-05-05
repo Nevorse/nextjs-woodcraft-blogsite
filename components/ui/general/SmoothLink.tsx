5ab03e46f72e7a6c9e489a5c8b92b66e63541b0d
Nevorse <selcuk0463@gmail.com>
2026-05-05 14:47:39 +0300

    m

--- components/ui/general/SmoothLink.tsx
+++ components/ui/general/SmoothLink.tsx
@@ -1,21 +1,21 @@
-import { Suspense } from "react";
+// import { Suspense } from "react";
 import Link from "next/link";
 import SmoothLinkClient from "./SmoothLinkClient";
 
 type SmoothLinkProps = React.ComponentPropsWithoutRef<typeof Link>;
 
-function Fallback({ href, className, children }: Pick<SmoothLinkProps, "href" | "className" | "children">) {
-  return (
-    <a href={href.toString()} className={className}>
-      {children}
-    </a>
-  );
-}
+// function Fallback({ href, className, children }: Pick<SmoothLinkProps, "href" | "className" | "children">) {
+//   return (
+//     <a href={href.toString()} className={className}>
+//       {children}
+//     </a>
+//   );
+// }
 
 export default function SmoothLink(props: SmoothLinkProps) {
   return (
-    <Suspense fallback={<Fallback href={props.href} className={props.className}>{props.children}</Fallback>}>
+    // <Suspense fallback={<Fallback href={props.href} className={props.className}>{props.children}</Fallback>}>
       <SmoothLinkClient {...props} />
-    </Suspense>
+    // </Suspense>
   );
 }
